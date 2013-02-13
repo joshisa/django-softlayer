b6b72a4e14ae37c9d0bf83349b129b582f7402ad
BakanovKirill <kirill.bakanov@gmail.com>
2013-02-13 12:10:33 +0200

    fixed imports

--- src/django_softlayer/management/commands/syncstatic.py
+++ src/django_softlayer/management/commands/syncstatic.py
@@ -1,24 +1,13 @@
 ### -*- coding: utf-8 -*- ####################################################
 
 import os
-import datetime
-import sys
-import object_storage
 import fnmatch
 from optparse import make_option
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
-from django.core.exceptions import ImproperlyConfigured
 from django.core.files import File
-from django.utils.encoding import force_unicode
-from django.utils._os import safe_join
-from django.utils.importlib import import_module
-
-from pytils.translit import slugify
 from django_softlayer import SoftLayerStorage
-
-from music.utils import parse_item
 
 class Command(BaseCommand):
     '''Walks the directory specified in MEDIA_FOLDER and uploads mp3 files to storage'''
@@ -43,7 +32,6 @@ class Command(BaseCommand):
         for root, dirnames, filenames in os.walk(mediaroot):
             for filename in fnmatch.filter(filenames, mask):
                 file_name = os.path.abspath(os.path.join(root, filename))
-        
                 relative_name = os.path.relpath(file_name, mediaroot)
                 
                 with open(file_name, 'rb') as _file:
