37ad0b77c69887cf2fca7211f01bd20014863b0e
BakanovKirill <kirill.bakanov@gmail.com>
2013-08-06 16:46:36 +0300

    fixed command arguments

--- src/django_softlayer/management/commands/get_missing_files.py
+++ src/django_softlayer/management/commands/get_missing_files.py
@@ -13,29 +13,26 @@ class Command(BaseCommand):
         make_option('--app',
                     dest='app',
                     help='Django application to get models from. Example: myApp'),
-        make_option('--models',
-                    dest='models',
-                    help='Model names list. Example: User,Album,Track'),
-        make_option('--fields',
-                    dest='fields',
-                    help='File fields list. Example: file,image,audio_track'),
+        make_option('--model_fields',
+                    dest='model_fields',
+                    help='String containing dot separated model name and field name\n Example: User.file'),
     )
 
 
-    def handle(self, app, models, fields, *args, **options):
+    def handle(self, app, model_fields, *args, **options):
         self.index = 1
+        models = []
         if hasattr(settings, 'CMD_MISSING_FILES_SETTINGS'):
-            cmd_settings=settings.CMD_MISSING_FILES_SETTINGS
+            cmd_settings = settings.CMD_MISSING_FILES_SETTINGS
             app = cmd_settings.get('app', False)
-            models = cmd_settings.get('models', False)
-            fields = cmd_settings.get('fields', False)
+            model_fields = cmd_settings.get('model_fields', False)
+            for obj in model_fields:
+                models.append(obj.split('.'))
 
         if not app:
-            raise CommandError('You must specify --app option')
-        if not models:
-            raise CommandError('You must specify --models option')
-        if not fields:
-            raise CommandError('You must specify --fields option')
+            raise CommandError('You must specify --app option. Example: myApp')
+        if not model_fields:
+            raise CommandError('You must specify --model_fields option. Example: Model.field')
 
         def printTrack(obj, reason):
             print "%s. Instance: %s\n\tID: %s\n\tReason: %s" % \
@@ -52,18 +49,14 @@ class Command(BaseCommand):
                 except:
                     printTrack(obj, 'FILE NOT FOUND')
             print '----------------------------------Done--------------------------------------'
-
-        model_names = models.split(',')
-        field_names = fields.split(',')
-
-        for model_name in model_names:
-            model = get_model(app, model_name)
-            for field_name in field_names:
-                try:
-                    model._meta.get_field(field_name)
-                except FieldDoesNotExist:
-                    continue
-                get_missing_files(model, field_name)
+        for model_field in models:
+            model = get_model(app, model_field[0])
+            field_name = model_field[1]
+            try:
+                model._meta.get_field(field_name)
+            except FieldDoesNotExist:
+                continue
+            get_missing_files(model, field_name)
 
 
 
